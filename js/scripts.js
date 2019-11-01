b9078421e54901037b0f302b5e96158645142c3c
Celeste Morton <creneemorton@gmail.com>
2019-11-01 10:23:26 -0700

    use a loop to establish whether Beep or Boop or BeepBoop will return based off the number submitted by the FE user

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 // Business logic goes here:
 
-var pingPong = function (number) {
+var beepBoop = function (number) {
     for (var number = 1; number <= 30; number++) {
         var result = "";
         if (number % 3 === 0) {
-            result = "Ping";
+            result = "Beep";
         }
         if (number % 5 === 0) {
-            result += "Pong";
+            result += "Boop";
         }
         console.log(result || number);
     }
@@ -16,14 +16,16 @@ var pingPong = function (number) {
 // Front end logic goes here:
 
 $(document).ready(function () {
-    $("form#ping-pong").submit(function (event) {
+    $("form#beep-boop").submit(function (event) {
         event.preventDefault();
         var number = parseInt($("input#number").val());
-        var result = pingPong(number);
+        var result = beepBoop(number);
         $("#result").text(result);
 
-        if (number % 3 === 0) {
-            result = ("Ping");
+        for (var number = 1; number <= 30; number++, result = "") {
+            if (!(number % 3)) result += "Beep";
+            if (!(number % 5)) result += "Boop";
+            console.log(result || number);
         }
 
 
